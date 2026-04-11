a09469331032b396913abb2484fbb4f2278dc734
Vishal <vishalgupta8359@gmail.com>
2026-04-12 02:00:33 +0530

    expand the token expire duration for good user experience

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -17,7 +17,7 @@ const env = {
 
   jwt: {
     secret: process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    expiresIn: process.env.JWT_EXPIRES_IN || "365d",
   },
 
   redis: {
