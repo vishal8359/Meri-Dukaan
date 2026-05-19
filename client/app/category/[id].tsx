0b104219a0e1d3b5c2e749e67eb077b185a5b556
Vishal <vishalgupta8359@gmail.com>
2026-05-19 22:06:25 +0530

    pd top change

--- client/app/category/[id].tsx
+++ client/app/category/[id].tsx
@@ -800,7 +800,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingTop: 80,
+    paddingTop: 79,
   },
   emptyEmoji: {
     fontSize: 48,
